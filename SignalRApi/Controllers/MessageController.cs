b191c1f34bc3a39be7d9284709d8e0b08162bb2c
serkancan <serkancan@DESKTOP-9CS0Q1K>
2024-03-13 20:30:06 +0300

    Bölüm 23 Sonu

---
+++ SignalRApi/Controllers/MessageController.cs
@@ -0,0 +1,62 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SignalR.BusinessLayer.Abstract;
+using SignalR.DtoLayer.MessageDto;
+using SignalR.EntitiyLayer.Entities;
+using SignalR.EntityLayer.Entities;
+
+namespace SignalRApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MessageController : ControllerBase
+    {
+        private readonly IMessageService _MessageService;
+        private readonly IMapper _mapper;
+
+        public MessageController(IMessageService MessageService, IMapper mapper)
+        {
+            _MessageService = MessageService;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public IActionResult MessageList()
+        {
+            var values = _MessageService.TGetListAll();
+            return Ok(values);
+        }
+
+        [HttpPost]
+        public IActionResult CreateMessage(CreateMessageDto createMessageDto)
+        {
+            createMessageDto.Status = false;
+            createMessageDto.MessageSendDate = DateTime.Now;
+            _MessageService.TAdd(_mapper.Map<Message>(createMessageDto));
+            return Ok("Mesaj Kısmı Başarılı Bir Şekilde Eklendi.");
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteMessage(int id)
+        {
+            var value = _MessageService.TGetByID(id);
+            _MessageService.TDelete(value);
+            return Ok("Mesaj Alanı Silindi");
+        }
+
+        [HttpPut]
+        public IActionResult UpdateMessage(UpdateMessageDto updateMessageDto)
+        {
+            _MessageService.TUpdate(_mapper.Map<Message>(updateMessageDto));
+            return Ok("Mesaj Alanı Güncellendi");
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetMessage(int id)
+        {
+            var value = _MessageService.TGetByID(id);
+            return Ok(value);
+        }
+    }
+}
