6b74aa720fdebb070128d59cc73376c4c5b04f2e
serkancan <serkancan@DESKTOP-9CS0Q1K>
2024-03-13 22:13:06 +0300

    Proje Seviyesinde AUthenticate

--- SignalRWebUI/Program.cs
+++ SignalRWebUI/Program.cs
@@ -1,12 +1,19 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.Authorization;
 using SignalR.DataAccessLayer.Concrete;
 using SignalR.EntityLayer.Entities;
 
 var builder = WebApplication.CreateBuilder(args);
 
+var requireAUthoricePolicy = new AuthorizationPolicyBuilder().RequireAuthenticatedUser().Build();
+
 // Add services to the container.
 builder.Services.AddDbContext<SignalRContext>();
 builder.Services.AddIdentity<AppUser, AppRole>().AddEntityFrameworkStores<SignalRContext>();
-builder.Services.AddControllersWithViews().AddRazorRuntimeCompilation();
+builder.Services.AddControllersWithViews(opt =>
+{
+    opt.Filters.Add(new AuthorizeFilter(requireAUthoricePolicy));    
+}).AddRazorRuntimeCompilation();
 
 builder.Services.AddHttpClient();
 builder.Services.AddHttpContextAccessor();
